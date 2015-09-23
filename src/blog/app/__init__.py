dbdf5d0e3bad5efd443d79b637659951df2e98ea
Benjie Jiao <benjiao12@gmail.com>
2015-09-23 11:26:44 +0800

    Added route definitions in __init__.py

--- src/blog/app/__init__.py
+++ src/blog/app/__init__.py
@@ -2,6 +2,32 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-        return "Hello workshop!"
+    return "Hello workshop!"
+
+
+@app.route('/user')
+def user_list():
+    return "<h1>This is the user page</h1>"
+
+
+@app.route('/user/<username>')
+def user_profile(username):
+    return "<h1>This is the profile page of %s" % username
+
+
+@app.route('/post/<int:post_id>')
+def post_page(user_id):
+    return "<h1>This is the profile ID page of %s" % user_id
+
+
+@app.route('/login', methods=['GET'])
+def login_page():
+    return "LOGIN!"
+
+
+@app.route('/login', methods=['POST'])
+def login_submit():
+    return "LOGIN!"
---
+++ src/blog/config.py
@@ -0,0 +1,6 @@
+class Config(object):
+    APP_NAME = "Awesome Blog"
+
+    DEBUG = True
+    LOG_FILE = 'debug.log'
+    LOG_FORMAT = '%(asctime)s %(levelname)-8s %(message)s'
